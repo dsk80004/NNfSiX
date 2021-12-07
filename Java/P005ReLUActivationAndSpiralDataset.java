7b3db32aa03a0e6f3cff7a0e9b5d45fc19e1b97d
dsk80004 <48543299+dsk80004@users.noreply.github.com>
2021-12-07 16:46:19 -0600

    relu activation function fixed
    on line 150, the relu activation function is currently inversed, only returning negative numbers and not positive ones

--- Java/P005ReLUActivationAndSpiralDataset.java
+++ Java/P005ReLUActivationAndSpiralDataset.java
@@ -147,7 +147,7 @@ public class P005ReLUActivationAndSpiralDataset {
             output = new double[inputs.length][inputs[0].length];
             for (int i = 0; i < output.length; i++) {
                 for (int j = 0; j < output[0].length; j++) {
-                    if (inputs[i][j] > 0) {
+                    if (inputs[i][j] < 0) {
                         output[i][j] = 0;
                     } else {
                         output[i][j] = inputs[i][j];
